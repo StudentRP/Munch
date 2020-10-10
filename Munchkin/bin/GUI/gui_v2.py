f41c9e278624e3a5627e83b0d15269e8a568d47a
R <rryphllps@gmail.com>
2020-10-10 12:20:27 +0100

    build of player select

--- Munchkin/bin/GUI/gui_v2.py
+++ Munchkin/bin/GUI/gui_v2.py
@@ -1,8 +1,8 @@
 """ GUI as class to be imported into the working script. Accommodated for scope issues with get()
 
 requiremets:
-    starting gui (welcome screen)
-    player number select
+    starting gui (welcome screen) timed delay to open
+    number of players select
     name and sex, sex maybe check box..
     main game loop window
 
@@ -12,10 +12,13 @@ requiremets:
 from tkinter import *
 
 
-class StartScreen():
+
+class StartScreen:
+    """This will be the start screen, will need door pic."""
     def __init__(self):
         self.root = Tk()# main root window. Current thinking = need spawns of Toplevel for main game loop
         self.title = self.root.title("Munchkin World")
+        self.screensize = self.root.geometry("600x300")
         self.frame = Frame(self.root)
         self.frame.pack()
         self.fl = Label(self.frame, text="hi class")
@@ -30,6 +33,23 @@ class StartScreen():
         Label(self.root, text=f"{self.enter.get()}").pack()
 
 
-r = StartScreen # instance
+# r = StartScreen() # instance
 
-r() # calling the instance
+# r() # calling the instance
+
+
+class NumOfPlayers:
+    """dropdown menu for player select"""
+    def __init__(self):
+        self.root = Tk()
+        self.title = self.root.title("Player select")
+        self.screensize = self.root.geometry("600x300")
+        self.frame = Frame(self.root)
+        self.frame.pack()
+        self.fl = Label(self.frame, text="Please select number of players")
+        self.fl.pack(side=TOP, fill=X)
+
+        self.mainloop = mainloop()
+
+n = NumOfPlayers()
+
