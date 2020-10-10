278d5ea8e027224886a9d7c84fe9f948a06fac1c
R <rryphllps@gmail.com>
2020-10-10 19:19:38 +0100

    testGUI class

--- Munchkin/bin/GUI/gui_v2.py
+++ Munchkin/bin/GUI/gui_v2.py
@@ -17,6 +17,7 @@ class MainScreen:
     """This will be the start screen, will need door pic."""
     def __init__(self):
         self.root = Tk()# main root window. Current thinking = need spawns of Toplevel for main game loop
+        self.topmenu = Menu(self.root)
         self.title = self.root.title("Munchkin World")
         self.screensize = self.root.geometry("600x300")
         self.frame = Frame(self.root)
@@ -25,7 +26,7 @@ class MainScreen:
         self.fl.pack()
         self.ent = Entry()
         self.ent.pack()
-        self.but = Button(self.root, text="get text", command=self.getobj)
+        self.but = Button(self.root, text="get text", command=self.get_obj)
         self.but.pack()
         self.mainloop = mainloop()
 
@@ -58,3 +59,24 @@ class NumOfPlayers:
 #     for attrib, value in player_info:
 
 
+class TestWin:
+    def __init__(self, person='JB', level=0): # P(n) from player class will be instances for this
+        self.root = Tk()
+        self.title = self.root.title("Test Window")
+        self.label = Label(self.root, text='Should show name and level')
+        self.label.grid(column=0, row=0, columnspan=2)
+
+        self.name = Label(self.root, text='Name')
+        self.name.grid(column=0, row=1)
+        self.nameval = Label(self.root, text=f'{person}') #location for player name
+        self.nameval.grid(column=1, row=1)
+
+        self.level = Label(self.root, text='Level')
+        self.level.grid(column=0, row=2)
+        self.levelval = Label(self.root, text=f'{level}')
+        self.levelval.grid(column=1, row=2)
+
+        self.root.mainloop()
+
+
+p = TestWin()
