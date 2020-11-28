5d09e51351cbec0debe27c77aad9222e7b88bd10
R <rryphllps@gmail.com>
2020-11-28 12:58:26 +0000

    18/10/20 update gui gui interface working custom label color

---
+++ Munchkin/bin/GUI/gui_v3.py
@@ -0,0 +1,18 @@
+""" New format gui
+imports all scripts
+
+Main tasks:
+    create main start/exit window
+    create player select toplevel
+    create player name/sex entery toplevel
+    create main toplevel window with 3 main frames(nav, player info, action window)
+
+sub tasks:
+    save/load capability
+
+
+"""
+
+import tkinter
+
+
